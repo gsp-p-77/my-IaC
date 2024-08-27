0a6ea392328d360365ad91c5aa57ff288770238e
Sparwald Gero <gero.sparwald@elektrobit.com>
2024-08-27 14:47:48 +0200

    Remove minikube, add docker engine vServer

---
+++ servers/02-docker-engine/vServer/provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit the script if any command fails
+set -e
+
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add docker compose-plugin
+sudo apt-get install -y docker-compose-plugin
+
+#Add ubuntu user to docker group
+usermod -aG docker ubuntu
+
+#Install unzip
+sudo apt install unzip -y
+
